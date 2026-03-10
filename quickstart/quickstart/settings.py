fe21123e7735e266565dabbe7cd35b6d9c589780
AlfredoB14 <alfredobarranco14@gmail.com>
2026-03-09 19:15:31 -0600

    FIX DEL TONTO QUE SUBIÓ EL HOSTNAME

--- quickstart/quickstart/settings.py
+++ quickstart/quickstart/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,7 +77,7 @@ WSGI_APPLICATION = 'quickstart.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django_mongodb_backend',
-        'HOST': 'mongodb+srv://visumedadmin:txIjSWD5vyN2hsKt@visumedcluster.ajwybdz.mongodb.net/?appName=VisuMedCluster',
+        'HOST': os.environ.get('HOSTKEY'),
         'NAME': 'quickstart',
     },
 }
